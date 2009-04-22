07ace0b5fcbf36b1b497149b69dff4c36c8de424
Hong Jiang <hjiang@67-218-106-191.cust.layer42.net>
2009-04-22 08:20:27 -0700

    properly load ruby-mode

--- _emacs.d/hjiang-customization.el
+++ _emacs.d/hjiang-customization.el
@@ -23,9 +23,28 @@
 (setq next-line-add-newlines nil)
 
 (require 'php-mode)
-(require 'ruby-mode)
 (autoload 'markdown-mode "markdown-mode.el"
   "Major mode for editing Markdown files" t)
+
+;; ruby                                                                         
+;; based on http://www.rubygarden.org/Ruby/page/show/InstallingEmacsExtensions  
+;;                                                                              
+
+(add-to-list 'load-path "~/.emacs.d/ruby-mode")
+
+(autoload 'ruby-mode "ruby-mode" "Mode for editing ruby source files")
+(add-to-list 'auto-mode-alist '("\\.rb$" . ruby-mode))
+(add-to-list 'interpreter-mode-alist '("ruby" . ruby-mode))
+(autoload 'run-ruby "inf-ruby" "Run an inferior Ruby process")
+(autoload 'inf-ruby-keys "inf-ruby"
+  "Set local key defs for inf-ruby in ruby-mode")
+(add-hook 'ruby-mode-hook
+          '(lambda () (inf-ruby-keys)))
+;; If you have Emacs 19.2x or older, use rubydb2x                              
+(autoload 'rubydb "rubydb3x" "Ruby debugger" t)
+;; uncomment the next line if you want syntax highlighting                     
+(add-hook 'ruby-mode-hook 'turn-on-font-lock)
+
 
 ;; Language modes
 (add-to-list 'auto-mode-alist '("CMakeLists\\.txt\\'" . cmake-mode))
