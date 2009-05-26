59ad1fa8d3c4c653a8ba20f15f81694f6a8f2225
Hong Jiang <hong@hjiang.net>
2009-05-25 19:05:33 -0700

    load rails mod

--- _emacs.d/common.el
+++ _emacs.d/common.el
@@ -28,4 +28,7 @@
           '(lambda ()
              (define-key yaml-mode-map "\C-m" 'newline-and-indent)))
 
+(add-to-list 'load-path "~/.emacs.d/vendor/rinari")
+(require 'rinari)
+
 (server-start)
