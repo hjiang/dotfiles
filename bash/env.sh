125f88d3c7c86a1bb572f31e2e6ec4e03b043f82
Hong Jiang <hjiang@hjiang-macbookpro15.local>
2009-08-18 01:36:11 +0800

    adjusted bash configs and added zsh configs

---
+++ bash/env.sh
@@ -0,0 +1,5 @@
+# For MacPorts
+export PATH=/opt/local/bin:/opt/local/sbin:$PATH
+export MANPATH=/opt/local/share/man:$MANPATH
+
+export PATH=$PATH:~/code/scripts
---
+++ zshrc
@@ -0,0 +1,5 @@
+. ~/code/dotfiles/zsh/env
+. ~/code/dotfiles/zsh/config
+. ~/code/dotfiles/zsh/aliases
+
+
