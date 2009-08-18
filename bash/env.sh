d76e50a4277b8efbb03a9533dd1ce3e6fccabe70
Hong Jiang <hjiang@google.com>
2009-08-18 12:39:24 +0800

    added google-specific configuration

--- bash/env.sh
+++ bash/env.sh
@@ -3,3 +3,23 @@ export PATH=/opt/local/bin:/opt/local/sbin:$PATH
 export MANPATH=/opt/local/share/man:$MANPATH
 
 export PATH=$PATH:~/code/scripts:~/bin
+
+export EDITOR="emacsclient -a vim"
+export VISUAL=$EDITOR
+export SVN_EDITOR=$EDITOR
+export GIT_EDITOR=$EDITOR
+
+case `hostname` in
+  *.google.com) export IN_GOOGLE=1 ;;
+  *) export IN_GOOGLE=0 ;;
+esac
+
+if [ $IN_GOOGLE -eq 1 ]; then
+  export GOOGLE_USE_CORP_SSL_AGENT=true
+  export P4DIFF="diff"
+  export P4MERGE=/home/build/public/eng/perforce/mergep4.tcl
+  export P4EDITOR=${EDITOR}
+  export P4CONFIG=.p4config
+  export PATH=$HOME/opt/bin:$HOME/depot_tools:/opt/bin:$PATH:/auto/buildpublic/google/tools:/home/build/nonconf/google3/tools:$HOME/.gem/ruby/1.8/bin
+  /home/build/static/projects/loas/setup-corp-loasd.sh -q
+fi
