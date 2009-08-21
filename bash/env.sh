4ba0db3d9357f0171749ca81af3c0271f14c7d84
Hong Jiang <hjiang@google.com>
2009-08-21 16:33:14 +0800

    modified zsh config

--- bash/env.sh
+++ bash/env.sh
@@ -4,7 +4,7 @@ export MANPATH=/opt/local/share/man:$MANPATH
 
 export PATH=$PATH:~/code/scripts:~/bin
 
-export EDITOR="emacsclient -a vim"
+export EDITOR=vim
 export VISUAL=$EDITOR
 export SVN_EDITOR=$EDITOR
 export GIT_EDITOR=$EDITOR
