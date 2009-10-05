dbd1c26bd9f6de1804c6fc24770f3f99f11804fe
Hong Jiang <hjiang@google.com>
2009-10-05 16:59:30 -0700

    adjust env setup code

--- bash/env.sh
+++ bash/env.sh
@@ -20,6 +20,7 @@ if [ $IN_GOOGLE -eq 1 ]; then
   export P4MERGE=/home/build/public/eng/perforce/mergep4.tcl
   export P4EDITOR=${EDITOR}
   export P4CONFIG=.p4config
-  export PATH=$HOME/opt/bin:$HOME/depot_tools:/opt/bin:$PATH:/auto/buildpublic/google/tools:/home/build/nonconf/google3/tools:$HOME/.gem/ruby/1.8/bin
-  /home/build/static/projects/loas/setup-corp-loasd.sh -q
+  export PATH=$HOME/opt/bin:$HOME/depot_tools:/opt/bin:$PATH:/auto/build/public/google/tools:/home/build/nonconf/google3/tools:$HOME/.gem/ruby/1.8/bin:/home/build/static/projects/overlayfs
+  test -e /home/build/static/projects/loas/setup-corp-loasd.sh && /home/build/static/projects/loas/setup-corp-loasd.sh -q
+  # export USE_CANARY_BLAZE=head
 fi
