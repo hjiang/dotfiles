b899418916e4087ff660604901337ede1a3320cd
Hong Jiang <hjiang@li89-36.members.linode.com>
2009-11-13 01:02:28 +0000

    added clojure env setup

--- bash/env.sh
+++ bash/env.sh
@@ -1,8 +1,8 @@
 # For MacPorts
-export PATH=/opt/local/bin:/opt/local/sbin:$PATH
+PATH=/opt/local/bin:/opt/local/sbin:$PATH
 export MANPATH=/opt/local/share/man:$MANPATH
 
-export PATH=~/code/scripts:~/bin:~/.gem/ruby/1.8/bin:$PATH
+PATH=~/code/scripts:~/bin:~/.gem/ruby/1.8/bin:$PATH
 
 export EDITOR=vim
 export VISUAL=$EDITOR
@@ -10,6 +10,12 @@ export SVN_EDITOR=$EDITOR
 export GIT_EDITOR=$EDITOR
 
 export LC_ALL=en_US.UTF-8
+
+# Clojure
+# Install using ~/code/scripts/install_clojure.sh
+export CLOJURE_EXT=~/.clojure
+PATH=$PATH:~/.clojure/launchers/bash
+alias clj=clj-env-dir
 
 case `hostname` in
   *.google.com) export IN_GOOGLE=1 ;;
@@ -22,7 +28,9 @@ if [ $IN_GOOGLE -eq 1 ]; then
   export P4MERGE=/home/build/public/eng/perforce/mergep4.tcl
   export P4EDITOR=${EDITOR}
   export P4CONFIG=.p4config
-  export PATH=$HOME/opt/bin:$HOME/depot_tools:/opt/bin:$PATH:/auto/build/public/google/tools:/home/build/nonconf/google3/tools:$HOME/.gem/ruby/1.8/bin:/home/build/static/projects/overlayfs
+  PATH=$HOME/opt/bin:$HOME/depot_tools:/opt/bin:$PATH:/auto/build/public/google/tools:/home/build/nonconf/google3/tools:$HOME/.gem/ruby/1.8/bin:/home/build/static/projects/overlayfs
   test -e /home/build/static/projects/loas/setup-corp-loasd.sh && /home/build/static/projects/loas/setup-corp-loasd.sh -q
   # export USE_CANARY_BLAZE=head
 fi
+
+export PATH
