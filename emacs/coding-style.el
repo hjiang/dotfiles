36af230d31d454d19dd6db53512486a15ace4a8f
江宏 <kmldqj@gmail.com>
2011-05-04 16:37:23 +0800

    Update emacs config

--- emacs/coding-style.el
+++ emacs/coding-style.el
@@ -22,135 +22,15 @@
 ;; Also useful, perhaps, is fill mode.  In theory, when you use M-q,
 ;; it will word-wrap your comment paragraphs.
 
-;; We'll use 2 everywhere for Python
-(setq py-indent-offset 2)
-
-;; Indent for JavaScript
-(setq js2-basic-offset 2)
-
-;; Never use tabs, and indent by 4 properly.
-(defun hjiang-set-perl-style ()
-  (interactive)
-  (setq perl-indent-level 4)
-  (setq perl-continued-statement-offset 4)
-  (setq perl-label-offset -4)
-  (setq indent-tabs-mode nil))
-
-(add-hook 'perl-mode-hook 'hjiang-set-perl-style)
-
-(setq ruby-indent-level 2)
-
-(setq thrift-indent-level 4)
-
-(setq css-indent-level 4)
-
-(setq-default tab-width 2)
-
-(setq auto-mode-alist
-      (cons '("\\.less$" . css-mode) (cons '("\\.js$" . js2-mode)
-       (cons '("\\.[ch]$" . c++-mode) auto-mode-alist)))) ; .c,.h in C++ mode
-
-(defconst hjiang-c-style
-  `((c-tab-always-indent . t)
-    (c-recognize-knr-p . nil)
-    (c-enable-xemacs-performance-kludge-p . t) ; speed up indentation in XEmacs
-    (c-basic-offset . 2)
-    (c-comment-only-line-offset . 0)
-    (c-hanging-braces-alist . (
-			       (defun-open after)
-			       (defun-close before after)
-			       (class-open after)
-			       (class-close before after)
-			       (inline-open after)
-			       (inline-close before after)
-			       (block-open after)
-			       (block-close . c-snug-do-while)
-			       (extern-lang-open after)
-			       (extern-lang-close after)
-			       (statement-case-open after)
-			       (substatement-open after)
-			       ))
-    (c-hanging-colons-alist . (
-			       (case-label)
-			       (label after)
-			       (access-label after)
-			       (member-init-intro before)
-			       (inher-intro)
-			       ))
-    (c-hanging-semi&comma-criteria
-     . (c-semi&comma-no-newlines-for-oneline-inliners
-	c-semi&comma-inside-parenlist
-	c-semi&comma-no-newlines-before-nonblanks))
-    (c-indent-comments-syntactically-p . nil)
-    (comment-column . 40)
-    (c-cleanup-list . (brace-else-brace
-		       brace-elseif-brace
-		       brace-catch-brace
-		       empty-defun-braces
-		       defun-close-semi
-		       list-close-comma
-		       scope-operator))
-    (c-offsets-alist . (
-			(arglist-intro . ++)
-                        (func-decl-cont . ++)
-                        (member-init-intro . ++)
-                        (inher-intro . ++)
-			(comment-intro . 0)
-			(arglist-close . c-lineup-arglist)
-			(topmost-intro . 0)
-			(block-open . 0)
-			(inline-open . 0)
-			(substatement-open . 0)
-                        (statement-cont
-                         .
-                         (,(when (fboundp 'c-no-indent-after-java-annotations)
-                             'c-no-indent-after-java-annotations)
-                          ,(when (fboundp 'c-lineup-assignments)
-                             'c-lineup-assignments)
-                          ++))
-			(label . /)
-			(case-label . +)
-			(statement-case-open . +)
-			(statement-case-intro . +) ; case w/o {
-			(access-label . /)
-			(innamespace . 0)
-			))
-    )
-  "Hong Jiang's C/C++ Programming Style")
-
-(defun hjiang-set-c-style ()
-  (interactive)
-  (setq indent-tabs-mode nil)
-  (c-add-style "hjiang" hjiang-c-style t))
-
-(defun hjiang-make-newline-indent ()
-  (interactive)
-  (define-key c-mode-base-map "\C-m" 'newline-and-indent)
-  (define-key c-mode-base-map [ret] 'newline-and-indent))
-
-(defun hjiang-c-mode-common-hook ()
-  "Customization for C/C++ mode"
-  (hjiang-set-c-style)
-  (hjiang-make-newline-indent)
-  (add-hook 'write-file-hooks 'delete-trailing-whitespace t)
-  (setq show-trailing-whitespace t)
-  (highlight-80+-mode t))
+(load-file "${HOME}/code/onycloud/tools/emacs/coding-style.el")
+(setq auto-mode-alist (cons '("\\.[ch]$" . c++-mode)
+                            auto-mode-alist)) ; .c,.h in C++ mode
 
 (defun hjiang-disallow-tab-indent ()
   "Don't use tabs to indent."
   (setq indent-tabs-mode nil))
 
-
-(add-hook 'c-mode-common-hook 'hjiang-c-mode-common-hook)
-(add-hook 'c-mode-common-hook 'hjiang-make-newline-indent)
-
-(add-hook 'clojure-mode 'hjiang-disallow-tab-indent)
-
-(setq-default indent-tabs-mode nil)
-
-(defun set-java-style ()
-  (setq c-basic-offset 4))
-(add-hook 'java-mode-hook 'set-java-style)
+(add-hook 'c-mode-common-hook 'code-hook)
 
 (provide 'hjiang-coding-style)
 
