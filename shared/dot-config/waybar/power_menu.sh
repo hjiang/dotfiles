56be6d8b1ad25981d5e1e509d21441e5248b8ff6
Hong Jiang <hjiang@users.noreply.github.com>
2025-12-03 18:14:37 +0800

    add missing power menu file

---
+++ shared/dot-config/waybar/power_menu.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+
+# Power menu options
+options="Lock Screen
+Logout
+Shutdown
+Reboot"
+
+# Show menu and get selection
+selected=$(printf "%s" "$options" | vicinae dmenu --no-section --no-metadata --no-quick-look)
+
+# Execute action based on selection
+case "$selected" in
+    "Lock Screen")
+        loginctl lock-session
+        ;;
+    "Logout")
+        hyprctl dispatch exit
+        ;;
+    "Shutdown")
+        loginctl poweroff
+        ;;
+    "Reboot")
+        loginctl reboot
+        ;;
+esac
